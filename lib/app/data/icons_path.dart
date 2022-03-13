1b011a26766ce4ca9d7e13ed0c78fdcb6f503b9c
fxt0706 <fxt0706@gmail.com>
2022-03-13 17:24:24 +0800

    update - icon path file

---
+++ lib/app/data/icons_path.dart
@@ -0,0 +1,13 @@
+// 图标路径
+
+const iconPlanetPath = 'assets/icons/planet-ring-2-line.svg';
+const iconArticlePath = 'assets/icons/article-line.svg';
+const iconRightPath = 'assets/icons/chevron-right-circle-line.svg';
+const iconPenPath = 'assets/icons/edit-pen-4-line.svg';
+const iconFlowerPath = 'assets/icons/flower-2-line.svg';
+const iconHeartPath = 'assets/icons/heart-line.svg';
+const iconImagePath = 'assets/icons/image-circle-line.svg';
+const iconListBoxPath = 'assets/icons/list-box-line.svg';
+const iconMusicPath = 'assets/icons/music-line.svg';
+const iconSettingsPath = 'assets/icons/settings-cog-line.svg';
+const iconTvPath = 'assets/icons/tv-old-line.svg';
