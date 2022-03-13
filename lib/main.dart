972fd4f42655d824ce2f886c3bf4c4ed216d69b6
fxt0706 <fxt0706@gmail.com>
2022-03-13 17:49:41 +0800

    update - set main.dart spec

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 import 'package:get/get.dart';
 
@@ -6,10 +8,25 @@ import 'app/routes/app_pages.dart';
 
 void main() {
   runApp(
-    GetMaterialApp(
-      title: "Application",
-      initialRoute: AppPages.INITIAL,
-      getPages: AppPages.routes,
+    ScreenUtilInit(
+      designSize: const Size(324, 720),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: () => GetMaterialApp(
+        title: "Bog岛",
+        initialRoute: AppPages.INITIAL,
+        getPages: AppPages.routes,
+        defaultTransition: Transition.fadeIn,
+        enableLog: true,
+        localizationsDelegates: const [
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+        supportedLocales: const [
+          Locale('zh', 'CH'),
+        ],
+      ),
     ),
   );
 }
