a915ff7a52d41caca98b559f2ca9a188496a2602
fxt0706 <fxt0706@gmail.com>
2022-03-24 22:17:51 +0800

    update - lazyput fix

--- lib/app/modules/post_edit/bindings/post_edit_binding.dart
+++ lib/app/modules/post_edit/bindings/post_edit_binding.dart
@@ -1,6 +1,7 @@
 import 'package:bog_island/app/modules/post_edit/providers/image_upload_provider.dart';
 import 'package:bog_island/app/modules/post_edit/providers/post_content_provider.dart';
 import 'package:bog_island/app/modules/setting/controllers/setting_controller.dart';
+import 'package:bog_island/app/modules/setting/providers/cookie_add_provider.dart';
 import 'package:get/get.dart';
 
 import '../controllers/post_edit_controller.dart';
@@ -19,6 +20,9 @@ class PostEditBinding extends Bindings {
     );
     Get.lazyPut<SettingController>(
       () => SettingController(),
+    );
+    Get.lazyPut<CookieAddProvider>(
+      () => CookieAddProvider(),
     );
   }
 }
