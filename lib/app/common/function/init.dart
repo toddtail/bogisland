1961b1dce8cb51b9cff4bd6608dfc523f05738cb
ephf <fxt0706@gmail.com>
2022-03-28 15:03:48 +0800

    update - foce portait

--- lib/app/common/function/init.dart
+++ lib/app/common/function/init.dart
@@ -1,11 +1,16 @@
 import 'package:bog_island/app/modules/home/bindings/home_binding.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get_storage/get_storage.dart';
 
 initApp() async {
   WidgetsFlutterBinding.ensureInitialized();
   HomeBinding().dependencies();
-  // await GetStorage.init();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitDown,
+    DeviceOrientation.portraitUp
+  ]);
+  await GetStorage.init();
   await Future.delayed(const Duration(milliseconds: 400));
   return true;
 }
