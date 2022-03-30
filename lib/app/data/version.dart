6aca6c9dcefd1aeb1a79c8cce719950521ae7ac3
fxt0706 <fxt0706@gmail.com>
2022-03-30 22:02:33 +0800

    update - version data

--- lib/app/data/version.dart
+++ lib/app/data/version.dart
@@ -1,4 +1,4 @@
-const appVersionCode = 2;
-const appVersionName = '0.0.2 - 测试版';
+const appVersionCode = 3;
+const appVersionName = '0.0.3 - 测试版';
 const appVersionContent = 'Bog 岛第三方客户端测试版';
 const appVersionTime = '2022-3-30';
